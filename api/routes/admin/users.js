4c50171ff6171ce3b609a99f42e2f6fe1ded1ff5
GeethKavinda <kavindag.18@cse.mrt.ac.lk>
2021-01-14 20:26:23 +0530

    1

--- api/routes/admin/users.js
+++ api/routes/admin/users.js
@@ -10,30 +10,15 @@ router.get('/', (req,res) => {
 } )
 
 router.put('/:userId', (req,res) => {
-<<<<<<< HEAD
-    user.update()
-=======
     console.log(req.body);
     console.log("uP");
     console.log(req.params.userId);
-    user.update({officerID:req.params.userId, ...req.body})
->>>>>>> 1fcd2cdb30d0968d5de33860d1dd7670cfb09df8
+    user.update({officerId:req.params.userId, ...req.body})
     .then(data => res.send(data))
     .catch(err => console.log(err))
 } )
 
 router.post('/', (req,res) => {
-<<<<<<< HEAD
-    user.create()
-    .then(data => res.send(data))
-    .catch(err => console.log(err))
-} )
-
-router.delete('/:userId', (req,res) => {
-    user.destroy()
-    .then(data => res.send(data))
-    .catch(err => console.log(err))
-=======
     console.log(req.body);
     user.create(req.body)
     .then(data => res.send(data))
@@ -45,7 +30,6 @@ router.delete('/:userId', (req,res) => {
     user.delete(req.params.userId)
     .then(() => res.send("Success"))
     .catch(err => res.send(err))
->>>>>>> 1fcd2cdb30d0968d5de33860d1dd7670cfb09df8
 } )
 
 module.exports = router;
