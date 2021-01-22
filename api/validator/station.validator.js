faa915bf2a0dd1d6cb54b79cbd6aa32b392163f1
poorna2152 <poorn2152@gmail.com>
2021-01-22 13:30:34 +0530

    Fix station validator

--- api/validator/station.validator.js
+++ api/validator/station.validator.js
@@ -1,15 +1,15 @@
-const Joi = require('@hapi/joi');
+// const Joi = require('@hapi/joi');
 
-const validateStationSchema = Joi.object({
-		stationID: Joi.number().integer().min(1).required(),
+// const validateStationSchema = Joi.object({
+// 		stationID: Joi.number().integer().min(1).required(),
 
-		name: Joi.string().min(2).max(100).required(),
+// 		name: Joi.string().min(2).max(100).required(),
 
-		location: Joi.string().alphanum().min(2).max(100).required(),
+// 		location: Joi.string().alphanum().min(2).max(100).required(),
 
-		type: Joi.string().min(3).max(10).required(),
+// 		type: Joi.string().min(3).max(10).required(),
 
-		contactNo: Joi.string().max(10).required()
-	});
+// 		contactNo: Joi.string().max(10).required()
+// 	});
 	
-module.exports = {validateStationSchema}
+// module.exports = {validateStationSchema}
