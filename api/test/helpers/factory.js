19907fcc7a222210f47b49dfe1a87b0a7f47714a
poorna2152 <poorn2152@gmail.com>
2021-02-26 09:05:36 +0530

    ADD -helpers

--- api/test/helpers/factory.js
+++ api/test/helpers/factory.js
@@ -13,5 +13,98 @@ exports.createStation =  (count = 1) => {
             }
         )
     }
+    if(count == 1) return stations[0]
     return stations;
+}
+
+exports.createAmmoModel = (count = 1) => {
+    let ammoModels = [];
+    for(let i =0; i < count; i++){
+        ammoModels.push(
+            {
+                name: faker.lorem.words(3),
+                description: faker.lorem.words(10)
+            }
+        )
+    }
+    
+    if(count == 1) return ammoModels[0]
+    
+    return ammoModels;
+}
+
+
+exports.createWeaponModel = (count = 1) => {
+    let weaponModels = [];
+    for(let i =0; i < count; i++){
+        weaponModels.push(
+            {
+                name: faker.lorem.words(3),
+                description: faker.lorem.words(10)
+            }
+        )
+    }
+    if(count == 1) return weaponModels[0]
+    
+    return weaponModels;
+}
+
+
+exports.createRequestAmmunition = (count = 1,ammoModel, requestID = -1) => {
+    let ammoModelIDs = ammoModel.map(item => item.ammoModelID) 
+    let requestAmmo = [];
+
+    for(let i =0; i < count; i++){
+        requestAmmo.push(
+            {
+                name: faker.lorem.words(3),
+                amount: faker.random.number(1000),
+                ammoModelID: ammoModelIDs[i]
+            }
+        )
+    }
+    if(requestID != -1){
+        requestAmmo = requestAmmo.map(item => item.requestID = requestID)
+    }
+    if(count == 1) return requestAmmo[0]
+    
+    return requestAmmo;
+}
+
+
+exports.createRequestWeapon = (count = 1,weaponModel,requestID = -1) => {
+    let weaponModelIDs = weaponModel.map(item => item.weaponModelID) 
+    let requestWeapon = [];
+
+    for(let i =0; i < count; i++){
+        requestWeapon.push(
+            {
+                name: faker.lorem.words(3),
+                amount: faker.random.number(1000),
+                ammoModelID: weaponModelIDs[i]
+            }
+        )
+    }
+    if(requestID != -1){
+        requestWeapon = requestWeapon.map(item => item.requestID = requestID)
+    }
+    if(count == 1) return requestWeapon[0]
+    
+    return requestWeapon;
+}
+
+exports.createRequest = (station,weaponModel,ammoModel) => {
+    let requests = [];
+    
+    for(let i =0; i < count; i++){
+        requests.push(
+            {
+                date: faker.company.companyName(), 
+                comments: faker.address.city(),
+                state: STATION_TYPES[Math.floor(Math.random() * STATION_TYPES.length)],
+                stationID: stationID 
+            }
+        )
+    }
+
 }
